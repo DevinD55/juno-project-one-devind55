381fef7f7476833524b9caee274dbbf6be48255d
devin-dunn <devineamondunn@live.com>
2023-02-19 16:24:19 -0500

    working on js to add comments

---
+++ blog.js
@@ -0,0 +1,11 @@
+const commentName = document.querySelector('forms.comment-form.comment-name.input');
+const commentEmail = document.querySelector('input[name=comment-email]');
+const commentText= document.querySelector('input[name=comment-text]');
+
+
+document.querySelector("#comment-form").addEventListener('submit', function(event){
+    event.preventDefault();
+    console.log('Form submitted!')
+
+
+})
