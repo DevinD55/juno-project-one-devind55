11bc83d1426410a0bbdaed51aca3802460fb6563
devin-dunn <devineamondunn@live.com>
2023-02-20 13:31:36 -0500

    tried to add hamburger menu and ability to add comments to blog. Both not working.

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 document.querySelector("#close").addEventListener('click', function(){
     document.querySelector("#popup").style.display = "none";
 });
+
+const hamburger = document.querySelector("#hamburger");
+
+hamburger.addEventListener('click', function(){
+    const menu = document.querySelector(".open-menu");
+    menu.classList.toggle('open-menu.classList.display="none"');
+
+    menu.classList.toggle('open-menu.classList.visibility="visible"');
+})
